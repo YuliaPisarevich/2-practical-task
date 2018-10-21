bd96ce966c313a82f1743cff60fdebdb657d2fd8
YuliaPisarevich_github <yulik.997@gmail.com>
2018-10-22 00:15:30 +0300

    Update 1_task/src/DragonLogic.java

--- 1_task/src/DragonLogic.java
+++ 1_task/src/DragonLogic.java
@@ -1,9 +1,14 @@
 public class DragonLogic {
+    private static final int TWO_HEAD_AGE = 200;
+    private static final int ONE_HEAD_AGE = 300;
+    private static final int EYES_NUMBER = 2;
+    private static final int START_HEADS = 3;
+
     public static int solveNumOfHeads(int age) {
-        int numOfHeads = 3;
-        if (age < 200) {
+        int numOfHeads = START_HEADS;
+        if (age < TWO_HEAD_AGE) {
             numOfHeads = headsAgeUpTo200(age, numOfHeads);
-        } else if (age >= 200 && age < 300) {
+        } else if (age >= TWO_HEAD_AGE && age < ONE_HEAD_AGE) {
             numOfHeads = headsAgeUpTo300(age, numOfHeads);
         } else {
             numOfHeads = headsAgeAfter300(age, numOfHeads);
@@ -11,20 +16,20 @@ public class DragonLogic {
         return numOfHeads;
     }
 
-    public static int headsAgeUpTo200(int age, int numOfHeads) {
+    private static int headsAgeUpTo200(int age, int numOfHeads) {
         return numOfHeads + age * 3;
     }
 
-    public static int headsAgeUpTo300(int age, int numOfHeads) {
+    private static int headsAgeUpTo300(int age, int numOfHeads) {
         return numOfHeads + 199 * 3 + (age - 199) * 2;
     }
 
-    public static int headsAgeAfter300(int age, int numOfHeads) {
+    private static int headsAgeAfter300(int age, int numOfHeads) {
         return numOfHeads + 199 * 3 + 100 * 2 + (age - 299);
     }
 
     public static int solveNumOfEyes(int numOfHeads) {
-        return numOfHeads * 2;
+        return numOfHeads * EYES_NUMBER;
     }
 
 }
